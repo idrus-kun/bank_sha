691af9aa3224bd8f3c9c2c724d1d3f6b232d701f
idrus-kun <109605613+idrus-kun@users.noreply.github.com>
2022-11-29 18:30:09 +0700

    edited 10.11

--- lib/blocs/auth/auth_event.dart
+++ lib/blocs/auth/auth_event.dart
@@ -18,7 +18,13 @@ class AuthCheckEmail extends AuthEvent {
 class AuthRegister extends AuthEvent {
   final SignUpFormModel data;
   const AuthRegister(this.data);
+  @override
+  List<Object> get props => [data];
+}
 
+class AuthLogin extends AuthEvent {
+  final SignInFormModel data;
+  const AuthLogin(this.data);
   @override
   List<Object> get props => [data];
 }
